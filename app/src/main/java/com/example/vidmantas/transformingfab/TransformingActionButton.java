f4145525f981dd3cb91542c6872dcf56a99ef4c4
Vidmantas Kerbelis <vidkrb@telesoftas.com>
2015-09-24 16:40:52 +0300

    Initial commit.

---
+++ app/src/main/java/com/example/vidmantas/transformingfab/TransformingActionButton.java
@@ -0,0 +1,24 @@
+package com.example.vidmantas.transformingfab;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * @author Vidmantas Kerbelis (vkerbelis@yahoo.com) on 2015-09-24.
+ */
+public class TransformingActionButton extends FrameLayout {
+
+    public TransformingActionButton(Context context) {
+        super(context);
+    }
+
+    public TransformingActionButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TransformingActionButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
